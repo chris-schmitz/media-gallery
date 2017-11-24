b632f495ca7a481210a873a2536d7774c9992954
chris-schmitz <schmitz.chris@gmail.com>
2017-11-24 08:19:52 -0600

    Adds base project

---
+++ src/renderer/components/Gallery.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+    div.gallery-container
+        p gallery
+</template>
+
+<script>
+    export default {
+        data () {
+            return {}
+        }
+    }
+</script>
+
+<style lang='scss' scoped>
+    .gallery-container{}
+</style>
